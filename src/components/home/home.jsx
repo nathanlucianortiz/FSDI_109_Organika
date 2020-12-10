b034e43d7f27e41bc4aa11c2d9595cd37e0f7743
AlinaMari Ortiz <Alina@AlinaMaris-MBP.attlocal.net>
2020-12-09 17:42:15 -0800

    Fix the bug

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return <h1>Welcome to Organika store</h1>;
+    return <h1>Welcome to Organika</h1>;
   }
 }
 
